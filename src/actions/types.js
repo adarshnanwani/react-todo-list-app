52674fd5856d59cfb669b51587ea9c31456bb527
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-05 17:05:14 +0530

    Add action types and files for actions and reducers.

---
+++ src/actions/types.js
@@ -0,0 +1,26 @@
+// User action types
+export const SIGN_UP = 'SIGN_UP';
+export const LOGIN = 'LOGIN';
+export const CHANGE_PASSWORD = 'CHANGE_PASSWORD';
+export const RESET_PASSWORD = 'RESET_PASSWORD';
+export const GET_USER_DATA = 'GET_USER_DATA';
+export const SET_USER_DATA = 'SET_USER_DATA';
+
+// Todo Lists action types
+export const ADD_TODO_LIST = 'ADD_TODO_LIST';
+export const DELETE_TODO_LIST = 'DELETE_TODO_LIST';
+export const DELETE_TODO_LISTS = 'DELETE_TODO_LISTS';
+export const RENAME_TODO_LIST = 'RENAME_TODO_LIST';
+export const SHARE_TODO_LIST = 'SHARE_TODO_LIST';
+export const GET_SHARED_TODO_LIST = 'GET_SHARED_TODO_LIST';
+export const GET_ALL_TODO_LISTS = 'GET_ALL_TODO_LISTS';
+export const SET_ALL_TODO_LISTS = 'SET_ALL_TODO_LISTS';
+
+// Todo List Item action types
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const GET_ALL_ITEMS = 'GET_ALL_ITEMS';
+export const GET_ITEMS_FOR_A_TODO_LIST = 'GET_ITEMS_FOR_A_TODO_LIST';
+export const SET_ITEMS_FOR_A_TODO_LIST = 'SET_ITEMS_FOR_A_TODO_LIST';

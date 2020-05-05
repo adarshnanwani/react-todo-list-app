1090ce745ae608c121d14b0e0b01f29c0cd4af10
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-05 16:37:52 +0530

    Add signup code.

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,27 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useInput from '../../hooks/useInput';
+
 import './Signup.css';
 
 const Signup = () => {
+  const [name, changeName, resetName] = useInput('');
+  const [email, changeEmail, resetEmail] = useInput('');
+  const [password, changePassword, resetPassword] = useInput('');
+  const [cpassword, changeCpassword, resetCpassword] = useInput('');
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log({
+      name,
+      email,
+      password,
+      cpassword,
+    });
+    resetName();
+    resetEmail();
+    resetPassword();
+    resetCpassword();
+  };
+
   return (
     <div className='Signup'>
-      <form>
+      <form onSubmit={formSubmitHandler}>
         <h3>Sign Up</h3>
         <div>
           <label htmlFor='name'>Name:</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' value={name} onChange={changeName} />
         </div>
         <div>
           <label htmlFor='email'>Email:</label>
-          <input type='email' id='email' />
+          <input type='email' id='email' value={email} onChange={changeEmail} />
         </div>
         <div>
           <label htmlFor='password'>Password:</label>
-          <input type='password' id='password' />
+          <input
+            type='password'
+            id='password'
+            value={password}
+            onChange={changePassword}
+          />
         </div>
         <div>
           <label htmlFor='cpassword'>Confirm Password:</label>
-          <input type='password' id='cpassword' />
+          <input
+            type='password'
+            id='cpassword'
+            value={cpassword}
+            onChange={changeCpassword}
+          />
         </div>
         <input type='submit' value='Signup' />
         <div className='message'>

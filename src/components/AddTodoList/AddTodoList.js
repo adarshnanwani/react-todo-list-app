facb296d303b6addfb7a4e1a4388c0dffdad5366
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-06 11:35:30 +0530

    Add AddTodoList component.

---
+++ src/components/AddTodoList/AddTodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AddTodoList.css';
+
+const AddTodoList = () => {
+  return (
+    <div className='AddTodoList'>
+      <h3>Add Todo List</h3>
+      <form></form>
+    </div>
+  );
+};
+
+export default AddTodoList;
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import AddTodoList from '../AddTodoList/AddTodoList';
 import './Dashboard.css';
 
 const Dashboard = () => {
   return (
     <div className='Dashboard'>
       <section className='add-todo'>Add todo</section>
-      <section className='add-todo-list'>Add todo List</section>
+      <section className='add-todo-list'>
+        <AddTodoList />
+      </section>
       <section className='todo-list'>Todo list</section>
       <section className='master-todo-list'>Master todo list</section>
     </div>
